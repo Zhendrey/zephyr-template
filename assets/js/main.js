bc757f5dd6c83eb0a1d5b3b1553356a60a8e41ad
Yevhen <132243427+Zhendrey@users.noreply.github.com>
2025-04-13 17:42:39 -0700

    enhancing bookkng 💯

--- assets/js/main.js
+++ assets/js/main.js
@@ -164,30 +164,30 @@ async function findDepartureAirport(event){
 	const airportData = await getAirlinesData('airports');
 	const data = airportData[0].data;
 	const value = event.target.value.toLowerCase();
-	const directionLabel = event.target.closest(`label[for="direction"]`).querySelector(".airports");
-	const airportItem = directionLabel.querySelectorAll(".item__airport-name");
+	const airportsEl = event.target.closest(`label[for="direction"]`).querySelector(".airports");
+	const [...airportItem] = airportsEl.querySelectorAll(".airports__item");
+	const [...airportName] = airportsEl.querySelectorAll(".item__airport-name");
 	
 	const matchedAirport = data.filter(({departure})=>{
 		const {timezone, iata, airport} = departure;
+		const obj = {timezone, iata, airport};
 		const isSatisfied = [];
-		for (const key in {timezone, iata, airport}) {
-			const obj = {timezone, iata, airport};
+		for (const key in obj) {
 			if(!obj[key].toLowerCase().includes(value)) isSatisfied.push(false);
 			else isSatisfied.push(true);
 		}
 		return isSatisfied.some(condition=>condition)
 	}).map(({departure})=>departure.airport)
-	console.log(matchedAirport);
 	
-	directionLabel.classList.toggle("active", value !== ' ' && value !== '' && matchedAirport.length)
+	airportsEl.classList.toggle("active", value !== ' ' && value !== '' && matchedAirport.length)
 
-	for (const item of airportItem) {
-		if(!matchedAirport.includes(item.textContent)){
+	airportItem.forEach((item, index)=>{
+		if(!matchedAirport.includes(airportName[index].textContent)){
 			item.classList.remove("active")
 		}else{
 			item.classList.add("active")
 		}
-	}
+	})
 }
 rest.forEach(input=>input.addEventListener("input", findDepartureAirport))
 
@@ -205,7 +205,6 @@ function manageItem(action, parent, content,  className, el){
 		const planeIcon_SVG = `<svg viewBox="0 0 200 200" width="20" height="20" xmlns="http://www.w3.org/2000/svg" role="presentation"><path d="M178.081 41.973c-2.681 2.663-16.065 17.416-28.956 30.221c0 107.916 3.558 99.815-14.555 117.807l-14.358-60.402l-14.67-14.572c-38.873 38.606-33.015 8.711-33.015 45.669c.037 8.071-3.373 13.38-8.263 18.237L50.66 148.39l-30.751-13.513c10.094-10.017 15.609-8.207 39.488-8.207c8.127-16.666 18.173-23.81 26.033-31.62L70.79 80.509L10 66.269c17.153-17.039 6.638-13.895 118.396-13.895c12.96-12.873 26.882-27.703 29.574-30.377c7.745-7.692 28.017-14.357 31.205-11.191c3.187 3.166-3.349 23.474-11.094 31.167zm-13.674 42.469l-8.099 8.027v23.58c17.508-17.55 21.963-17.767 8.099-31.607zm-48.125-47.923c-13.678-13.652-12.642-10.828-32.152 8.57h23.625l8.527-8.57z"></path></svg>`
 		item.classList.add('item')
 		item.classList.add(className)
-		item.classList.add("active")
 		parent.appendChild(item);
 		item.insertAdjacentHTML("beforeend", 
 			`
